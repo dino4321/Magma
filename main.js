570e33630359944017df96f8ab3f6c02cab06e4f
Adekoya Daniel <adeyemiadekoya10@gmail.com>
2025-09-07 17:31:51 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('.connect');
+
+button.addEventListener('click', () => {
+    console.log(' clicked');
+});
+
+// function buttonClicked(){
+//     console.log('button clicked');
+//     // console.log (e);
+// }
